b6433999dfa497c3ad9b8007d05ca95fa49ef8bf
Eleash98 <67374695+Eleash98@users.noreply.github.com>
2021-07-10 21:10:21 +0200

    Delete DATALINK directory

--- DATALINK/Test.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_H_INCLUDED
-#define TEST_H_INCLUDED
-
-void TestSimplexStateMachine(void);
-void TestDuplexStateMachine(void);
-void TestDataLinkStateMachine(void);
-void Test_DL_PH(void);
-#endif // TEST_H_INCLUDED
--- DATALINK/UART.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART_H_INCLUDED
-#define UART_H_INCLUDED
-
-int UART_voidInit(char *PortAddress, u32 BaudRate);
-void UART_voidTransmit (int SerialID,char* tx_string);
-void UART_voidWriteArray(int SerialID,unsigned char* Arr, unsigned long int len);
-u8* UART_voidRead(int SerialID);
-#endif // UART_H_INCLUDED
